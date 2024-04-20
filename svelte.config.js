ac6d3ecf35ca017daefc95a7b7381180f7cd17ce
Mithilya <113309804+Mithilya@users.noreply.github.com>
2024-04-20 21:27:39 +0000

    updated

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,15 @@
 import adapter from '@sveltejs/adapter-netlify';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
-export default {
-  kit: {
-    adapter: adapter()
-  }
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: adapter()
+	}
 };
+
+export default config;
